9aaedc59e7977445dd00e4842020d12f8807e035
miedzygalaktycznygit <zamichal.789@gmail.com>
2024-09-08 23:11:03 +0200

    aktualizacja projektu

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,26 @@ function deleteNote(noteId){
         window.location.href = "/";
     });
 }
+
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const product = document.querySelectorAll(".product");
+    const pname = document.getElementsByTagName("h2");
+
+    for (let i = 0; i < pname.length; i++) {
+        let match = product[i].getElementsByTagName('h2')[0];
+
+        if (match) {
+            let textvalue = match.textContent || match.innerHTML;
+
+            if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+                // Show product and retain its styles
+                product[i].style.display = "flex";  // Set it back to flex instead of block
+            } else {
+                // Hide product
+                product[i].style.display = "none";
+            }
+        }
+    }
+};
+
